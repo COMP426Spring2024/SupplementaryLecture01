8f07d80a668f91d4af8b5de35c81edf90cb7cc84
Ketan Mayer-Patel <kmp@cs.unc.edu>
2024-02-05 13:25:58 -0500

    Demo'd arrow functions, functional programming.

--- rig.js
+++ rig.js
@@ -1,10 +1,21 @@
+let make_message_div = function (message) {
+    let message_div = document.createElement('div');
+    let message_h2 = document.createElement('h2');
+    message_h2.append(document.createTextNode(message));
+    message_div.append(message_h2);
+    return message_div;
+}
 
-
-let goodbye_mars_div = document.createElement('div');
-
-goodbye_mars_div.innerHTML = "<h2>Goodbye, Mars!</h2>";
 
 let rig_div = document.getElementById('rig');
 
-rig_div.append(goodbye_mars_div);
+let planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Pluto'];
 
+let greetings = ['Hello', 'Goodbye', 'What\'s Up', 'Word', 'Hey', 'Looking Good'];
+
+// Associate each greeting with a random number
+greetings = greetings.map(g => [g, Math.random()])
+                     .sort((a, b) => a[1] - b[1])
+                     .map(ag => ag[0]);
+
+planets.forEach((p, i) => rig_div.append(make_message_div(`${greetings[i%greetings.length]}, ${p}!`)));
